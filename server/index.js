65ed739748455ccdbbc3045c8c87fd31f391641e
Sergey Kotyushkin <sergey.kotyushkin@firstlinesoftware.com>
2018-09-07 10:49:48 +0300

    Initial foreman

--- server/index.js
+++ server/index.js
@@ -12,10 +12,10 @@ app.get('*', function(req, res) {
   res.sendFile(path.join(distDirPath, 'index.html'));
 });
 
-app.listen(3200, function(err) {
+app.listen(process.env.PORT, function(err) {
   if (err) {
-    return console.error(err);
+    return console.error('Server hasn\'t started', err);
   }
 
-  console.log('Listening at http://localhost:3200/');
+  console.log(`Listening at http://localhost:${process.env.PORT}/`);
 });
